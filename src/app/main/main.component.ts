78598dea79d03f7646f2321a34602c30a5c53708
Daniel Su <danielsu@Daniels-MacBook-Pro.local>
2019-12-24 11:03:32 -0500

    Update method to parsing text to use an array rather than manually splitting by paragraph tags

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -18,14 +18,10 @@ export enum KEY_CODE {
 
 export class MainComponent implements OnInit {
   textContents: string;
-  content: string;
   paragraphNumber: number;
   paragraphs: string[];
-  public query: string;
   selectedText: string = '';
   serverData: any;
-  simplifiedText: string = '';
-  chapterText: any[]
   backgroundcolor = "#f18973"
 
   constructor(private httpClient: HttpClient) {}
@@ -33,23 +29,17 @@ export class MainComponent implements OnInit {
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
     if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
-      this.nextPTag(true);
+      this.nextParagraph();
     }
 
     if (event.keyCode === KEY_CODE.LEFT_ARROW) {
-      this.nextPTag(false);
+      this.prevParagraph();
     }
-
-    if (event.keyCode === KEY_CODE.UP_ARROW) {
-      this.skipChap();
-    }
-
-    this.scroll('highlightText');
   }
 
   ngOnInit() {
     this.paragraphNumber = 0;
-    this.textContents = "No Text Detected"
+    this.textContents = "No Text Detected, navigate to bookshare and fetch the text using the chrome extension."
     this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
       console.log("Data Recieved..");
       console.log(data);
@@ -61,48 +51,67 @@ export class MainComponent implements OnInit {
 
       this.paragraphs = this.textContents.split("<p>");
       this.paragraphs.forEach(function(str, index) {
-          this[index] = str.replace("</p>", "");
+          str = str.replace("</p>", "");
+          str = str.replace("<p>", "");
+          str = str.replace("<h1>", "");
+          str = str.replace("</h1>", "");
+          this[index] = str;
         }, this.paragraphs
       );
+
+
+       // Skip to text and update selected text with current paragraph.
+      while (this.validParagraph() 
+        && this.paragraphs[this.paragraphNumber].includes("Chapter") 
+        && this.paragraphNumber < this.paragraphs.length - 1) {
+        this.paragraphNumber++;
+      }
+      this.selectedText = this.paragraphs[this.paragraphNumber];
     });
   }
 
-  removeHighlightedText() {
-    this.backgroundcolor = "#f18973"
-    const arr = this.textContents.split(' #target class="highlightText"');
-
-    //Trim last char of arr[0]
-    this.textContents = arr.join('')
+  nextParagraph() {
+    if (this.paragraphNumber < this.paragraphs.length - 1) {
+      this.paragraphNumber++;
+      this.updateDisplayedText();
+    } else {
+      console.log("End of book hit.");
+    }
   }
 
-  // Looks for the next paragraph text
-  nextPTag(next: boolean) {
-    this.removeHighlightedText()
-
-    this.paragraphNumber = next
-      ? this.paragraphNumber + 1 : this.paragraphNumber <= 1
-        ? 1 : this.paragraphNumber - 1;
-    var num = 0;
-    //Cannot have an open tag that exists within 7 of end since </p> required
-    for (var i = 0; i < this.textContents.length - 6; i++) {
-      if (this.textContents.substring(i, i + 2) == "<p") {
-        if (++num == this.paragraphNumber) {
-          //Find closing tag
-          for (var j = i + 3; j < this.textContents.length - 4; j++) {
-            if (this.textContents.substring(j, j + 4) == "</p>") {
-              this.selectedText = this.textContents.substring(i + 3, i + (j - i));
-
-              this.textContents = this.textContents.substring(0, i + 2) + ' #target class="highlightText"'
-                + this.textContents.substring(i + 2, this.textContents.length);
-              
-              return;
-            }
-          }
-          console.log("No closing tag found")
-          return; 
-        }
-      }
+  prevParagraph() {
+    if (this.paragraphNumber != 0) {
+      --this.paragraphNumber;
+      this.skipIrrelevantParagraphAndUpdate();
+    } else {
+      console.log("Beginning of book hit.");
     }
+  }
+
+
+  skipIrrelevantParagraphAndUpdate() {
+    while (this.validParagraph() 
+      && this.paragraphs[this.paragraphNumber].includes("Chapter") 
+      && this.paragraphNumber < this.paragraphs.length - 1) {
+      this.paragraphNumber++;
+    }
+    this.updateDisplayedText();
+  }
+
+  validParagraph() {
+    return !(this.paragraphNumber < 0 
+      || this.paragraphNumber >= this.paragraphs.length);
+  }
+
+  updateDisplayedText() {
+    if (!this.validParagraph()) return false;
+    this.selectedText = this.paragraphs[this.paragraphNumber];
+  }
+
+
+  reset() {
+    this.paragraphNumber = 0;
+    this.skipIrrelevantParagraphAndUpdate();
   }
 
   processText() {
@@ -110,57 +119,6 @@ export class MainComponent implements OnInit {
     this.selectedText = this.selectedText.replace(/\\/g, "");
     //Removes >
     this.selectedText = this.selectedText.replace(/>/g, "");
-  }
-
-  reset() {
-    this.paragraphNumber = 0;
-    this.nextPTag(true);
-  }
-
-  //ONLY A SEARCH, doesn't actually replace selectedText, Used for FindNextChap()
-  findNextPTag() {
-    //Cannot have an open tag that exists within 7 of end since </p> required
-    this.removeHighlightedText()
-    console.log(this.textContents.indexOf("#target class"))
-    var paragraphCount = 0
-    for (var i = 0; i < this.textContents.length - 6; i++) {
-      if (this.textContents.substring(i, i + 2) == "<p") {
-        paragraphCount++;
-        //Find closing tag
-        for (var j = i + 3; j < this.textContents.length - 4; j++) {
-          if (this.textContents.substring(j, j + 4) == "</p>") {
-            if (j - (i + 3) > 600) {
-              this.selectedText = this.textContents.substring(i + 3, j)
-              //Add highlight feature 
-              this.paragraphNumber = paragraphCount;
-              this.textContents = this.textContents.substring(0, i + 2) + ' #target class="highlightText"'
-                + this.textContents.substring(i + 2, this.textContents.length);
-              return new Array(this.textContents.substring(i + 3, i + (j - i)), paragraphCount + "");
-            }
-            else {
-              break;
-            }
-          }
-        }
-        //If gotten to this body, no closing tag found
-      }
-      //No more <paragraph> found
-    }
-    console.log("No more paragraph tags left")
-    return new Array();
-  }
-  //Scrolls the an element id on execution
-  scroll(className: string): void {
-    const elementList = document.querySelectorAll('.' + className);
-    if (elementList.length > 0) {
-      const element = elementList[0] as HTMLElement;
-      element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  }
-  skipChap() {
-    this.findNextPTag();
-    this.scroll('highlightText');
-
   }
 
 }
